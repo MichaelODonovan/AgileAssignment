47729c8b316f8e4b6fea80a0773ec815db96a22a
Michael O'Donovan <michael.odonovan3@mycit.ie>
2018-12-04 09:57:40 +0000

    test5

--- python.py
+++ python.py
@@ -1,6 +1,6 @@
 import datetime
 print("change3")
-print("second bash attempt")
+print("test attempt")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
